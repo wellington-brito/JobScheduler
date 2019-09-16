8d13587ed7d97fcc9e359e117738b9f3e8440106
Wellington Brito <wellington.brito@cromg.org.br>
2019-09-16 09:36:31 -0300

    initial commit

---
+++ app/src/main/java/com/example/jb/MyJobService.java
@@ -0,0 +1,21 @@
+package com.example.jb;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class MyJobService extends JobService {
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}
